acc6ffdeee6a7e2fb46654824117ac670e692718
lakshyalab <gandhi@innovatussystems.com>
2017-10-11 08:29:57 -0700

    first commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'test'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures test'
+long_description 'Installs/Configures test'
+version '0.1.0'
+issues_url 'https://github.com/lakshyalab/test/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/lakshyalab/test' if respond_to?(:source_url)
